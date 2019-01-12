6e78094876abf2fe838a83af6f93c378f5e6873d
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-01-12 18:41:24 -0500

    Now we are modifying the records on the server.

---
+++ gatsby-node.js
@@ -0,0 +1,110 @@
+
+const { resolveTemplate } = require('./machines/resolveTemplate')
+
+exports.onCreateNode = ({ node, getNode, actions, createNode }) => {
+
+    const kindMap = {
+        "standard": ({ node, getNode, actions }) => {
+            
+            const { createNodeField } = actions
+
+            const { frontmatter, id } = node
+            const { grade, domain_label, domain_number } = frontmatter
+            const code = `${frontmatter.class}${grade}${domain_label}${domain_number}`
+
+            createNodeField({
+                node,
+                name: 'standardCode',
+                value: code.toLowerCase(),
+            })
+
+        },
+        "page": ({ node, getNode, actions }) => {
+    
+        }
+    }
+
+    if( node.internal.type === `MarkdownRemark` ){
+
+        const { kind } = node.frontmatter
+
+        const chosenKind = kindMap[kind]
+
+        if(chosenKind && typeof chosenKind === "function"){
+            chosenKind({ node, getNode, actions })
+        }else{
+            console.log("Unknown Kind:", kind)
+        }
+
+    }
+
+}
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+
+}
+
+exports.createPages = ({ actions, graphql }) => {
+
+    const { createPage } = actions
+
+    const query = `
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        id
+                        fields{
+                            standardCode
+                        }
+                        frontmatter {
+                            kind
+                            path
+                            template
+                        }
+                    }
+                }
+            }
+        }
+    `
+
+    return graphql( query ).then( result => {
+
+        const { errors, data } = result
+
+        if( errors ){
+            errors.map( ( error ) => { console.error( error.toString() ) } )
+            return Promise.reject( errors )
+        }
+
+        data.allMarkdownRemark.edges.forEach(({ node })=>{
+
+            const { id, frontmatter, fields } = node
+            const { kind, path, template } = frontmatter
+
+            const component = resolveTemplate( template || kind )
+            
+            if(kind === "standard"){
+                createPage({
+                    id,
+                    path:`standard/${ fields.standardCode }`,
+                    kind,
+                    component,
+                    context:{ id, kind },
+                })
+            }else{
+                createPage({
+                    id,
+                    path,
+                    kind,
+                    component,
+                    context:{ id, kind },
+                })
+            }
+
+
+        })
+
+    })
+
+}
