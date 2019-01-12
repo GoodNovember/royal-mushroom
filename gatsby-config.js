fbc76f30a04bdebf7f15abb82004d7b2b8b100cd
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-01-12 18:56:22 -0500

    added the normal netlify plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,18 @@ const plugins = [
         }
     },
     'gatsby-transformer-remark', // process frontmatter in markdown files
-    'gatsby-plugin-netlify-cms' // always save the best for last
+    'gatsby-plugin-netlify-cms',
+    {
+        resolve:'gatsby-plugin-netlify-cms',
+        options:{
+            modulePath:`${__dirname}/src/cms/cms.js`,
+            stylesPath: `${__dirname}/src/cms/cms.sass`,
+            enableIdentityWidget: true,
+            publicPath: `admin`,
+            htmlTitle: `Thine Royal-Mushroom Content Manager`,
+        }
+    },
+    'gatsby-plugin-netlify'  // always save the best for last
 ]
 
 module.exports = { plugins }
