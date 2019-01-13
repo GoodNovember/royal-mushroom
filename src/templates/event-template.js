f67eb0775d63bb498955290475ad7e57506c6b2d
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-01-12 23:38:47 -0500

    Update Event Yaml to use default

--- src/templates/event-template.js
+++ src/templates/event-template.js
@@ -5,49 +5,58 @@ const Template = ({ data }) => RenderData( data )
 
 const RenderData = (props) => {
 
-    const { markdownRemark } = props
+    const { markdownRemark, eventYaml } = props
 
-    if ( markdownRemark ){
+    if (! eventYaml){
+        console.error("No eventYaml?!?")
+    }
+
+    if ( markdownRemark && eventYaml ){
         const { frontmatter, html } = markdownRemark
-        return ( <RenderBlog frontmatter={ frontmatter } html={ html } /> )
+        return ( <RenderBlog frontmatter={ frontmatter } html={ html } eventYaml={eventYaml} /> )
     }else{
         return ( 
             <div>
                 <div>PageTemplate render Error: MarkdownRemark is not renderable.</div>
-                <pre>
-                    <code>
-                        { JSON.stringify(props, null, '  ') }
-                    </code>
-                </pre>
+                <pre><code>{ JSON.stringify(props, null, '  ') }</code></pre>
             </div>
         )
     }
 }
 
-const RenderBlog = ({ frontmatter, html }) => {
+const RenderBlog = ({ frontmatter, html, eventYaml }) => {
     
-    const { title, date } = frontmatter
+    const { title, startDateTime, endDateTime, location, cost } = frontmatter
     const __html = html
 
     return (
         <div className="event">
-            <div className="event__title">{ title }</div>
-            <div className="event__date">{ date }</div>
+            <div className="event__title">Title: { title }</div>
+            <div className="event__start">Start: { startDateTime }</div>
+            <div className="event__end">End: { endDateTime }</div>
+            <div className="event__cost">Cost: { cost }</div>
+            <div className="event__location">Location: { location || eventYaml.eventDefaultLocation || null }</div>
             <div className="event__content" dangerouslySetInnerHTML={{ __html }}/>
         </div>
     )
 }
 
 export const pageQuery = graphql`
-  query($id: String!){
-    markdownRemark( id: {eq: $id} ){
-      html
-      frontmatter{
-        date(formatString:"MMMM DD, YYYY")
-        title
-      }
+    query($id: String!){
+        eventYaml{
+            eventDefaultLocation
+        }
+        markdownRemark( id: {eq: $id} ){
+            html
+            frontmatter{
+                title
+                cost
+                startDateTime
+                endDateTime
+                location
+            }
+        }
     }
-  }
 `
 
 export default Template
